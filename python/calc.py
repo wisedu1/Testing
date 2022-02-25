e758676e72ffd9c5459e3ed55abf3d994be36db8
wisedu1 <wisedu1@gmail.com>
2022-02-25 20:55:20 +0800

    init project

---
+++ python/calc.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# type hints 类型提示
+class Calc:
+
+    def add(self, a: int, b: int):
+        return a + b
+
+    def add1(self, a: int, b: int):
+        return a + b
+
+    def div(self, a, b):
+        return a / b
